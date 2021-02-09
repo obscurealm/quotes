16594e5c9080f489294623dabe7640b61af4f0b5
Yusuf <yusuf@madetech.com>
2021-02-09 14:35:17 +0000

    Update GET /api/quotes endpoint to return JSON content

--- api/test/acceptance/quotes.test.js
+++ api/test/acceptance/quotes.test.js
@@ -1,9 +1,10 @@
 import getQuotes from "../../../pages/api/quotes/index.js";
 
-describe("/api/quotes", () => {
-  it("returns a 200 if the method is GET", async () => {
+describe("GET /api/quotes", () => {
+  it("returns status 200", async () => {
     const response = {
       status: jest.fn().mockReturnValue({end: jest.fn()}),
+      json: jest.fn(),
     };
 
     const req = {
@@ -13,5 +14,40 @@ describe("/api/quotes", () => {
     await getQuotes(req, response);
 
     expect(response.status).toBeCalledWith(200);
+  });
+
+  it("returns a list of quotes", async () => {
+    const expectedResponse = {
+      data: {
+        quotes: [
+          {
+            timestamp: "1593013680",
+            quote: [
+              { name: "Ting", message: "Butt it’s wrong? *strokes beard*" },
+              {
+                name: "Yusuf",
+                message:
+                  "I don’t know if you are stroking my beard or your imaginary beard…",
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    const response = {
+      status: jest.fn().mockReturnValue({end: jest.fn()}),
+      json: jest.fn().mockReturnValue({end: jest.fn()}),
+    };
+
+    const req = {
+      method: "GET",
+    };
+
+    await getQuotes(req, response);
+
+    console.log(response.send);
+
+    expect(response.json).toBeCalledWith(expectedResponse);
   });
 });
--- pages/api/quotes/index.js
+++ pages/api/quotes/index.js
@@ -1,3 +1,20 @@
 export default (req, res) => {
   res.status(200);
+  res.json({
+    data: {
+      quotes: [
+        {
+          timestamp: "1593013680",
+          quote: [
+            {name: "Ting", message: "Butt it’s wrong? *strokes beard*"},
+            {
+              name: "Yusuf",
+              message:
+                "I don’t know if you are stroking my beard or your imaginary beard…",
+            },
+          ],
+        },
+      ],
+    },
+  });
 }
