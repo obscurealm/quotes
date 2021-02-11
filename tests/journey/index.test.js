c67b00a09974e3e2b351332c37e0e4fa5c2cb841
Yusuf <yusuf@madetech.com>
2021-02-11 18:10:01 +0000

    Refactor: Modify should() -> expect() assertions
    - journey test for home page
    
    Co-authored-by: Ting <wenting@madetech.com>

--- tests/journey/index.test.js
+++ tests/journey/index.test.js
@@ -1,12 +1,16 @@
 describe("User visits the home page", () => {
   it("displays the home page", () => {
     cy.visit("/");
-    cy.title().should("eq", "Home");
+    cy.title().should((title) => {
+      expect(title).to.equal("Home");
+    });
   });
 
   it("has a list of quotes", () => {
     cy.visit("/");
-    cy.contains('h1', 'Quotes');
-    cy.get('[data-cy=quote]').should('have.length.at.least', 1).and('be.visible')
+    cy.contains("h1", "Quotes");
+    cy.get("[data-cy=quote]").should(($div) => {
+      expect($div).have.length.of.at.least(1);
+    });
   });
 });
