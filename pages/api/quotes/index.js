e93d31b42c765b6e63c8d065d4acd50339300015
Yusuf <yusuf@madetech.com>
2021-02-11 13:56:26 +0000

    Rename keys in quote object
    - quote -> dialogue
    - name -> author
    - message -> text

--- pages/api/quotes/index.js
+++ pages/api/quotes/index.js
@@ -23,11 +23,11 @@ export function getListOfQuotes() {
   return [
     {
       timestamp: "1593013680",
-      quote: [
-        { name: "Ting", message: "Butt it’s wrong? *strokes beard*" },
+      dialogue: [
+        { author: "Ting", text: "Butt it’s wrong? *strokes beard*" },
         {
-          name: "Yusuf",
-          message:
+          author: "Yusuf",
+          text:
             "I don’t know if you are stroking my beard or your imaginary beard…",
         },
       ],
--- tests/acceptance/quotes.test.js
+++ tests/acceptance/quotes.test.js
@@ -45,11 +45,11 @@ describe("GET /api/quotes", () => {
           quotes: [
             {
               timestamp: "1593013680",
-              quote: [
-                { name: "Ting", message: "Butt it’s wrong? *strokes beard*" },
+              dialogue: [
+                { author: "Ting", text: "Butt it’s wrong? *strokes beard*" },
                 {
-                  name: "Yusuf",
-                  message:
+                  author: "Yusuf",
+                  text:
                     "I don’t know if you are stroking my beard or your imaginary beard…",
                 },
               ],
