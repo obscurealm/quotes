7ac56423cce14c301b3d6c0fe1db62950fbaa77e
Yusuf <yusuf@madetech.com>
2021-02-11 13:38:03 +0000

    Refactor: Abstract use case logic
    - GET /api/quotes endpoint

--- pages/api/quotes/index.js
+++ pages/api/quotes/index.js
@@ -3,19 +3,7 @@ export default (req, res) => {
     res.status(200);
     res.json({
       data: {
-        quotes: [
-          {
-            timestamp: "1593013680",
-            quote: [
-              { name: "Ting", message: "Butt it’s wrong? *strokes beard*" },
-              {
-                name: "Yusuf",
-                message:
-                  "I don’t know if you are stroking my beard or your imaginary beard…",
-              },
-            ],
-          },
-        ],
+        quotes: getListOfQuotes(),
       },
     });
   } else {
@@ -30,3 +18,19 @@ export default (req, res) => {
     });
   }
 };
+
+export function getListOfQuotes() {
+  return [
+    {
+      timestamp: "1593013680",
+      quote: [
+        { name: "Ting", message: "Butt it’s wrong? *strokes beard*" },
+        {
+          name: "Yusuf",
+          message:
+            "I don’t know if you are stroking my beard or your imaginary beard…",
+        },
+      ],
+    },
+  ];
+}
