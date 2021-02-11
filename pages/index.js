59c2e2c493af69b05c1b9e81365bb13050f7f1c3
Yusuf <yusuf@madetech.com>
2021-02-11 14:00:19 +0000

    Add home/index page
    - display list of quotes

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import { getListOfQuotes } from "./api/quotes";
+
+const Home = ({ quotes }) => {
+  return (
+    <>
+      <h1>Quotes</h1>
+      {quotes.map((quote, index) => (
+        <div key={index}>
+          <h2>{quote.timestamp}</h2>
+          {quote.dialogue.map((dialogue, index) => (
+            <p key={index}>
+              <strong>{dialogue.author}</strong>: {dialogue.text}
+            </p>
+          ))}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export const getStaticProps = () => {
+  const quotes = getListOfQuotes();
+
+  return {
+    props: {
+      quotes,
+    },
+  };
+};
+
+export default Home;
