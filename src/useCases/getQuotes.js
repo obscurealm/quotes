5a4304616c55b48a45994354e03ee06a9973a3e6
Yusuf <yusuf@madetech.com>
2021-02-12 15:38:24 +0000

    Update get quotes use case to return quotes
    Co-authored-by: Ting <wenting@madetech.com>

--- src/useCases/getQuotes.js
+++ src/useCases/getQuotes.js
@@ -1,5 +1,17 @@
 export default class GetQuotesUseCase {
   execute() {
-    return [];
+    return [
+      {
+        timestamp: "1593013680",
+        dialogue: [
+          { author: "Ting", text: "Butt it’s wrong? *strokes beard*" },
+          {
+            author: "Yusuf",
+            text:
+              "I don’t know if you are stroking my beard or your imaginary beard…",
+          },
+        ],
+      },
+    ];
   }
 }
--- tests/unit/useCases/getQuotes.test.js
+++ tests/unit/useCases/getQuotes.test.js
@@ -1,9 +1,21 @@
 import GetQuotesUseCase from "../../../src/useCases/getQuotes.js";
 
-describe("getQuotes", () => {
+describe("getQuotes use case", () => {
   it("returns an array response", () => {
     const getQuotes = new GetQuotesUseCase();
 
-    expect(getQuotes.execute()).toEqual([]);
+    expect(getQuotes.execute()).toEqual([
+      {
+        timestamp: "1593013680",
+        dialogue: [
+          { author: "Ting", text: "Butt it’s wrong? *strokes beard*" },
+          {
+            author: "Yusuf",
+            text:
+              "I don’t know if you are stroking my beard or your imaginary beard…",
+          },
+        ],
+      },
+    ]);
   });
 });
