406f5696864a9dda17bdd853fadd3678c1c3d3f1
Yusuf <yusuf@madetech.com>
2021-02-15 12:51:07 +0000

    Update Quotes gateway to retrieve markdown files

--- src/gateways/quotesGateway.js
+++ src/gateways/quotesGateway.js
@@ -1,5 +1,12 @@
+import fs from "fs";
+import { join } from "path";
+
 export default class QuotesGateway {
   constructor(files) {
-    this.quotesDirectory = files
+    this.quotesDirectory = files;
+  }
+
+  retrieveMarkdownFiles() {
+    return fs.readdirSync(join(process.cwd(), this.quotesDirectory));
   }
 }
--- tests/unit/gateways/quotesGateway.test.js
+++ tests/unit/gateways/quotesGateway.test.js
@@ -2,8 +2,14 @@ import QuotesGateway from "../../../src/gateways/quotesGateway.js";
 
 describe("quotes gateway", () => {
   it("can be constructed with a quotes directory", () => {
-    const gateway = new QuotesGateway('../fixtures/quotes');
+    const gateway = new QuotesGateway("../fixtures/quotes");
 
-    expect(gateway.quotesDirectory).toEqual('../fixtures/quotes');
+    expect(gateway.quotesDirectory).toEqual("../fixtures/quotes");
+  });
+
+  it("can return list of markdown files", () => {
+    const gateway = new QuotesGateway("tests/fixtures/quotes");
+
+    expect(gateway.retrieveMarkdownFiles()).toEqual(["1.md", "2.md"]);
   });
 });
