779652bc9fb2237bb085c52f6097f147639f07b6
Yusuf <yusuf@madetech.com>
2021-02-17 11:04:12 +0000

    Update convertMarkdownToHtml to sanitise response

--- tests/unit/utils/convertMarkdownToHtml.test.js
+++ tests/unit/utils/convertMarkdownToHtml.test.js
@@ -6,4 +6,10 @@ describe("convertMarkdownToHtml util", () => {
 
     expect(html).toEqual("<strong>yusuf</strong>");
   });
+
+  it("can return a sanitised HTML response", () => {
+    const html = convertMarkdownToHtml("<img src=x onerror=alert(1)//>");
+
+    expect(html).toEqual('<img src="x" />');
+  });
 });
--- utils/convertMarkdownToHtml.js
+++ utils/convertMarkdownToHtml.js
@@ -1,5 +1,8 @@
 import marked from "marked";
+import sanitizeHtml from 'sanitize-html';
 
 export default function convertMarkdownToHtml(markdown) {
-  return marked.parseInline(markdown, []);
+  return sanitizeHtml(marked.parseInline(markdown, []), {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+  });
 }
