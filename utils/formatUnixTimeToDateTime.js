e43ba8fda4b6461dbf78b4f01463d0b2f0b1d894
Yusuf <yusuf@madetech.com>
2021-02-18 18:43:56 +0000

    Refactor: Update date output to use Moment.js
    - formatUnixTimeToDateTime util file
    
    Co-authored-by: Ting <wenting@madetech.com>

--- utils/formatUnixTimeToDateTime.js
+++ utils/formatUnixTimeToDateTime.js
@@ -1,17 +1,8 @@
 import moment from 'moment';
 
 export default function formatUnixTimeToDateTime(timestamp) {
-  const date = new Date(timestamp * 1000);
-
-  const formattedDate = date.toLocaleDateString("en-GB", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
-
-  moment.locale("en-GB")
-
+  const date = moment.unix(timestamp).format("D MMMM YYYY")
   const time = moment.unix(timestamp).utc(true).format("h:mma")
 
-  return { date: formattedDate, time };
+  return { date, time };
 }
