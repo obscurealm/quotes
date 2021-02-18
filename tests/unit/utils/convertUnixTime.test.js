3b1b2045a47aa3cdf93e28290de11c8ec850fca7
Yusuf <yusuf@madetech.com>
2021-02-18 15:37:05 +0000

    Update formatUnixTimeToDateTime util
    - return formatted time (4:48 PM)

--- tests/unit/utils/convertUnixTime.test.js
+++ tests/unit/utils/convertUnixTime.test.js
@@ -2,8 +2,14 @@ import formatUnixTimeToDateTime from "../../../utils/formatUnixTimeToDateTime";
 
 describe("formatUnixTimeToDateTime util", () => {
   it("can return a formatted date", () => {
-    const date = formatUnixTimeToDateTime("1593013680");
+    const { date } = formatUnixTimeToDateTime("1593013680");
 
     expect(date).toBe("24 June 2020");
+  });
+
+  it("can return a formatted time", () => {
+    const { time } = formatUnixTimeToDateTime("1593013680");
+
+    expect(time).toBe("4:48 PM");
   });
 });
--- utils/formatUnixTimeToDateTime.js
+++ utils/formatUnixTimeToDateTime.js
@@ -1,9 +1,17 @@
 export default function formatUnixTimeToDateTime(timestamp) {
   const date = new Date(timestamp * 1000);
 
-  return date.toLocaleDateString("en-GB", {
+  const formattedDate = date.toLocaleDateString("en-GB", {
     day: "numeric",
     month: "long",
     year: "numeric",
   });
+
+  const time = date.toLocaleTimeString("en", {
+    hour12: true,
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return { date: formattedDate, time };
 }
