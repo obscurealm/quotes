4466db14548bee9efd425ea055f7a2a9ec8dffa9
Yusuf <yusuf@madetech.com>
2021-02-18 16:25:36 +0000

    Update Quote component to use formatted Unix time

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import convertMarkdownToHtml from "../../utils/convertMarkdownToHtml";
+import formatUnixTimeToDateTime from "../../utils/formatUnixTimeToDateTime";
 
 const Quote = ({quote}) => {
+  const { date, time } = formatUnixTimeToDateTime(quote.timestamp);
+
   return (
     <div data-cy="quote">
-      <h2>{quote.timestamp}</h2>
+      <h2>{date} at {time}</h2>
       {quote.dialogue.map((dialogue, index) => (
         <p key={index}>
           <strong>{dialogue.author}</strong>:{" "}
--- tests/unit/components/Quote.test.js
+++ tests/unit/components/Quote.test.js
@@ -4,13 +4,13 @@ import { render, screen } from "@testing-library/react";
 describe("Quote component", () => {
   it("can display the quote", async () => {
     const quote = {
-      timestamp: "123",
+      timestamp: "1613649600",
       dialogue: [{ author: "Yusuf", text: "speedieboi" }],
     };
 
     render(<Quote quote={quote} />);
 
-    expect(await screen.findByText("123")).toBeTruthy();
+    expect(await screen.findByText("18 February 2021 at 12:00 PM")).toBeTruthy();
     expect(await screen.findByText("Yusuf")).toBeTruthy();
     expect(await screen.findByText("speedieboi")).toBeTruthy();
   });
