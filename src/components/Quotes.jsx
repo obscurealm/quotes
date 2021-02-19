0d74c12c8e382b7f725916adb3cae3365e2dbe19
Wen Ting Wang <wenting@madetech.com>
2021-02-19 16:00:54 +0000

    Add Quotes component
    Co-authored-by: Yusuf <yusufsheiqh@users.noreply.github.com>

---
+++ src/components/Quotes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Quote from "./Quote";
+
+const Quotes = ({quotes}) => {
+  return (
+    <div data-testid="quotes">
+      {quotes.map((quote, index) => (
+        <Quote key={index} quote={quote} />
+      ))}
+    </div>
+  )
+}
+
+export default Quotes;
---
+++ tests/unit/components/Quotes.test.js
@@ -0,0 +1,21 @@
+import Quotes from "../../../src/components/Quotes";
+import { render, screen } from "@testing-library/react";
+
+describe("Quotes component", () => {
+  it("can display quotes", () => {
+    const quotes = [
+      {
+        timestamp: "1613649600",
+        dialogue: [{ author: "Yusuf", text: "speedieboi" }],
+      },
+      {
+        timestamp: "1614610800",
+        dialogue: [{ author: "Ting", text: "I don't know the meaning of the word evil." }],
+      }
+    ];
+
+    render(<Quotes quotes={quotes} />);
+
+    expect(screen.getByTestId("quotes").children).toHaveLength(2);
+  });
+})
