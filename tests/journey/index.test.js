63c43ae95b2e697b141694d7024da5a313339f11
Yusuf <yusuf@madetech.com>
2021-02-26 15:02:47 +0000

    Refactor: Merge multiple tests for journey tests

--- tests/journey/index.test.js
+++ tests/journey/index.test.js
@@ -1,14 +1,10 @@
 describe("User visits the home page", () => {
-  it("displays the home page", () => {
+  it("can display a list of quotes", () => {
     cy.visit("/");
     cy.title().should((title) => {
       expect(title).to.equal("Home");
     });
-  });
-
-  it("has a list of quotes", () => {
-    cy.visit("/");
-    cy.contains("h1", "Quotes");
+    cy.get("h1").contains("Quotes");
     cy.get("[data-cy=quote]").should(($div) => {
       expect($div).have.length.of.at.least(1);
     });
--- tests/journey/quote.test.js
+++ tests/journey/quote.test.js
@@ -1,7 +1,6 @@
 describe("User visits the quote page", () => {
   it("can display a single quote", () => {
     cy.visit("/quotes/1593013680");
-
     cy.title().should((title) => {
       expect(title).to.equal("Quote");
     });
