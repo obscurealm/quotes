9cbb51ad5e1bdfed0e58e4b0e7bc899fd4330540
Wen Ting Wang <wenting@madetech.com>
2022-02-07 17:27:13 +0000

    Refactor: Use beforeAll for NotionGateway tests
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- tests/unit/gateways/notionGateway.test.js
+++ tests/unit/gateways/notionGateway.test.js
@@ -19,65 +19,61 @@ Client.mockImplementation(() => {
   };
 });
 
-describe("notion gateway", () => {
-  it("can be constructed with a token", () => {
-    new NotionGateway("somerandomtoken", null);
+it("constructs with a token", () => {
+  new NotionGateway("somerandomtoken", null);
 
-    expect(Client).toBeCalledWith({ auth: "somerandomtoken" });
+  expect(Client).toBeCalledWith({ auth: "somerandomtoken" });
+});
+
+it("constructs with a page ID", () => {
+  const gateway = new NotionGateway(null, "pageId");
+
+  expect(gateway.pageId).toEqual("pageId");
+});
+
+describe("when retrieving a list of quotes", () => {
+  let quotes;
+
+  beforeAll(async () => {
+    list
+      .mockResolvedValueOnce({
+        object: "list",
+        results: firstPageResults,
+        next_cursor: "5",
+        has_more: true,
+      })
+      .mockResolvedValueOnce({
+        object: "list",
+        results: secondPageResults,
+        has_more: false,
+      });
+
+    const gateway = new NotionGateway("somerandomtoken", "pageId");
+
+    quotes = await gateway.retrieveQuotes();
   });
 
-  it("can be constructed with a page ID", () => {
-    const gateway = new NotionGateway(null, "pageId");
-
-    expect(gateway.pageId).toEqual("pageId");
-  });
-
-  describe("when retrieving a list of quotes", () => {
-    beforeEach(() =>
-      list
-        .mockResolvedValueOnce({
-          object: "list",
-          results: firstPageResults,
-          next_cursor: "5",
-          has_more: true,
-        })
-        .mockResolvedValueOnce({
-          object: "list",
-          results: secondPageResults,
-          has_more: false,
-        })
+  it("calls Notion API with a page ID", async () => {
+    expect(list).toBeCalledWith(
+      expect.objectContaining({ block_id: "pageId" })
     );
+  });
 
-    it("calls Notion API with a page ID", async () => {
-      const gateway = new NotionGateway("somerandomtoken", "pageId");
-
-      await gateway.retrieveQuotes();
-
-      expect(list).toBeCalledWith(
-        expect.objectContaining({ block_id: "pageId" })
-      );
-    });
-
-    it("returns a formatted list", async () => {
-      const gateway = new NotionGateway("somerandomtoken", "pageId");
-
-      const quotes = await gateway.retrieveQuotes();
-
-      expect(quotes).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            datetime: "21 Oct 2021 at 16:06",
-            dialogue: [
-              "Y: Good evening Tingker Bell! :tingker-bell:",
-              "T: Good evening Emperor King Yusuf! :emperor-king-yusuf:",
-            ],
-          }),
-          expect.objectContaining({
-            datetime: "7 Feb 2022 at 15:50",
-            dialogue: ["Y: uwu"],
-          }),
-        ])
-      );
-    });
+  it("returns a formatted list", async () => {
+    expect(quotes).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          datetime: "21 Oct 2021 at 16:06",
+          dialogue: [
+            "Y: Good evening Tingker Bell! :tingker-bell:",
+            "T: Good evening Emperor King Yusuf! :emperor-king-yusuf:",
+          ],
+        }),
+        expect.objectContaining({
+          datetime: "7 Feb 2022 at 15:50",
+          dialogue: ["Y: uwu"],
+        }),
+      ])
+    );
   });
 });
