f931482e58d23dcc1f172bba7f5152ff9138ccc2
Wen Ting Wang <wenting@madetech.com>
2022-02-07 15:02:18 +0000

    Add retrieveQuotes method to NotionGateway
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- src/gateways/notionGateway.js
+++ src/gateways/notionGateway.js
@@ -2,7 +2,11 @@ import { Client } from "@notionhq/client";
 
 export default class notionGateway {
   constructor(token, pageId) {
-    this.client = new Client({auth: token})
-    this.pageId = pageId
+    this.client = new Client({ auth: token });
+    this.pageId = pageId;
+  }
+
+  async retrieveQuotes() {
+    await this.client.blocks.children.list({ block_id: this.pageId });
   }
 }
--- tests/unit/gateways/notionGateway.test.js
+++ tests/unit/gateways/notionGateway.test.js
@@ -1,22 +1,42 @@
 import { Client } from "@notionhq/client";
 import NotionGateway from "../../../src/gateways/notionGateway.js";
 
-jest.mock("@notionhq/client", () => {
+jest.mock("@notionhq/client");
+
+const list = jest.fn().mockResolvedValue([]);
+
+Client.mockImplementation(() => {
   return {
-    Client: jest.fn()
-  }
-})
+    blocks: {
+      children: {
+        list: list,
+      },
+    },
+  };
+});
 
 describe("notion gateway", () => {
   it("can be constructed with a token", () => {
     new NotionGateway("somerandomtoken", null);
 
-    expect(Client).toBeCalledWith({ auth: "somerandomtoken" })
+    expect(Client).toBeCalledWith({ auth: "somerandomtoken" });
   });
 
   it("can be constructed with a page ID", () => {
     const gateway = new NotionGateway(null, "pageId");
 
-    expect(gateway.pageId).toEqual("pageId")
+    expect(gateway.pageId).toEqual("pageId");
+  });
+
+  describe("when retrieving a list of quotes", () => {
+    it("calls Notion API with a page ID", async () => {
+      const gateway = new NotionGateway("somerandomtoken", "pageId");
+
+      await gateway.retrieveQuotes();
+
+      expect(list).toBeCalledWith(
+        expect.objectContaining({ block_id: "pageId" })
+      );
+    });
   });
 });
