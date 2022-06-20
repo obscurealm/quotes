202e170b06099c413b438c874f441166092def6f
Yusuf <yusuf@madetech.com>
2022-06-20 12:12:14 +0100

    Add convertDateTimeToUtc util
    Co-authored-by: Ting <wenting@madetech.com>

---
+++ tests/unit/utils/convertDateTimeToUtc.test.js
@@ -0,0 +1,27 @@
+import convertDateTimeToUtc from "../../../utils/convertDateTimeToUtc";
+
+describe("convertDateTimeToUtc util", () => {
+  it("converts London date time to UTC", () => {
+    const utcTime = convertDateTimeToUtc("24 Jun 2020 at 16:48", "Europe/London");
+
+    expect(utcTime).toBe("2020-06-24T15:48:00Z");
+  });
+
+  it("converts Paris date time to UTC", () => {
+    const utcTime = convertDateTimeToUtc("24 Jun 2020 at 17:48", "Europe/Paris");
+
+    expect(utcTime).toBe("2020-06-24T15:48:00Z");
+  });
+
+  it("converts Tokyo date time to UTC", () => {
+    const utcTime = convertDateTimeToUtc("25 Jun 2020 at 00:48", "Asia/Tokyo");
+
+    expect(utcTime).toBe("2020-06-24T15:48:00Z");
+  });
+
+  it("converts date time to UTC with supplied format", () => {
+    const utcTime = convertDateTimeToUtc("24 Jun 2020 16:48", "Europe/London", "DD MMM YYYY HH:mm");
+
+    expect(utcTime).toBe("2020-06-24T15:48:00Z");
+  });
+});
---
+++ utils/convertDateTimeToUtc.js
@@ -0,0 +1,9 @@
+import moment from "moment-timezone";
+
+const convertDateTimeToUtc = (dateTime, timeZone, format = "DD MMM YYYY at HH:mm") => {
+  const utcTime = moment.tz(dateTime, format, timeZone).utc().format();
+
+  return utcTime;
+};
+
+export default convertDateTimeToUtc;
