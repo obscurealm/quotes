871694c91cef91dce4426835095118510efbf3b9
Wen Ting Wang <wenting@madetech.com>
2022-10-25 17:19:47 +0100

    Update Search component to filter quotes
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Search = ({}) => {
+const Search = ({ quotes, setQuotes }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filterQuotes = (searchTerm) =>
+    quotes.filter((quote) =>
+      quote.dialogue.some((message) =>
+        message.text.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+
   return (
     <>
-      <input data-testid="search"></input>
-      <button>Search</button>
+      <input data-testid="search" value={searchTerm} onChange={handleChange} />
+      <button onClick={() => setQuotes(filterQuotes(searchTerm))}>
+        Search
+      </button>
     </>
   );
 };
--- tests/unit/components/Search.test.js
+++ tests/unit/components/Search.test.js
@@ -1,6 +1,6 @@
 import Search from "../../../src/components/Search";
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import React, { useState } from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Search component", () => {
   it("displays the search button", () => {
@@ -13,5 +13,53 @@ describe("Search component", () => {
     render(<Search />);
 
     expect(screen.getByTestId("search")).toBeTruthy();
+  });
+
+  it("displays quotes with the search term", () => {
+    let quotes = [
+      {
+        timestamp: 1593013680,
+        dialogue: [
+          { author: "Ting", text: "Another one!" },
+          {
+            author: "Yusuf",
+            text: "What do you think of `data: data`?",
+          },
+        ],
+      },
+      {
+        timestamp: 1593013680,
+        dialogue: [
+          { author: "Ting", text: "_Hello!_" },
+          {
+            author: "Yusuf",
+            text: "Goodbye!",
+          },
+        ],
+      },
+    ];
+
+    const setQuotes = (updatedQuotes) => (quotes = updatedQuotes);
+
+    render(<Search quotes={quotes} setQuotes={setQuotes} />);
+
+    fireEvent.change(screen.getByTestId("search"), {
+      target: { value: "Hello!" },
+    });
+
+    fireEvent.click(screen.getByText("Search"));
+
+    expect(quotes).toEqual([
+      {
+        timestamp: 1593013680,
+        dialogue: [
+          { author: "Ting", text: "_Hello!_" },
+          {
+            author: "Yusuf",
+            text: "Goodbye!",
+          },
+        ],
+      },
+    ]);
   });
 });
