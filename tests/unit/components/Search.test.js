69db74eb0449be596a677fbaddf66ef9c32e93ae
Wen Ting Wang <wenting@madetech.com>
2022-10-26 11:56:43 +0100

    Refactor: Add describe for Search component filtering test

--- tests/unit/components/Search.test.js
+++ tests/unit/components/Search.test.js
@@ -15,51 +15,44 @@ describe("Search component", () => {
     expect(screen.getByTestId("search")).toBeTruthy();
   });
 
-  it("displays quotes with the search term", () => {
-    let quotes = [
-      {
-        timestamp: 1593013680,
-        dialogue: [
-          { author: "Ting", text: "Another one!" },
-          {
-            author: "Yusuf",
-            text: "What do you think of `data: data`?",
-          },
-        ],
-      },
-      {
-        timestamp: 1593013680,
-        dialogue: [
-          { author: "Ting", text: "_Hello!_" },
-          {
-            author: "Yusuf",
-            text: "Goodbye!",
-          },
-        ],
-      },
-    ];
+  describe("filters quotes", () => {
+    it("matching the search term", () => {
+      let quotes = [
+        {
+          timestamp: 1593013680,
+          dialogue: [
+            { author: "Ting", text: "Another one!" },
+            { author: "Yusuf", text: "What do you think of `data: data`?" },
+          ],
+        },
+        {
+          timestamp: 1593013680,
+          dialogue: [
+            { author: "Ting", text: "_Hello!_" },
+            { author: "Yusuf", text: "Goodbye!" },
+          ],
+        },
+      ];
 
-    const setQuotes = (updatedQuotes) => (quotes = updatedQuotes);
+      const setQuotes = (updatedQuotes) => (quotes = updatedQuotes);
 
-    render(<Search quotes={quotes} setQuotes={setQuotes} />);
+      render(<Search quotes={quotes} setQuotes={setQuotes} />);
 
-    fireEvent.change(screen.getByTestId("search"), {
-      target: { value: "Hello!" },
+      fireEvent.change(screen.getByTestId("search"), {
+        target: { value: "Hello!" },
+      });
+
+      fireEvent.click(screen.getByText("Search"));
+
+      expect(quotes).toEqual([
+        {
+          timestamp: 1593013680,
+          dialogue: [
+            { author: "Ting", text: "_Hello!_" },
+            { author: "Yusuf", text: "Goodbye!" },
+          ],
+        },
+      ]);
     });
-
-    fireEvent.click(screen.getByText("Search"));
-
-    expect(quotes).toEqual([
-      {
-        timestamp: 1593013680,
-        dialogue: [
-          { author: "Ting", text: "_Hello!_" },
-          {
-            author: "Yusuf",
-            text: "Goodbye!",
-          },
-        ],
-      },
-    ]);
   });
 });
