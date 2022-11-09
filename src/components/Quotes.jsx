9881830359ced9b504221eeb6f6007a9073e1640
Yusuf <yusuf@madetech.com>
2022-11-09 16:27:59 +0000

    Bugfix: Create new quotes array to sort in Quotes component

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -4,7 +4,7 @@ import Quote from "./Quote";
 const Quotes = ({ quotes }) => {
   return (
     <div data-testid="quotes">
-      {quotes
+      {[...quotes]
         .sort((quoteA, quoteB) => quoteA.timestamp - quoteB.timestamp)
         .map((quote, index) => (
           <Quote key={index} quote={quote} hasLink={true} />
