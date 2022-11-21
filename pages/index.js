b0c44718a6a0ab0fd2a8076d02c7243b9c751266
Wen Ting Wang <wenting@madetech.com>
2022-11-21 12:08:26 +0000

    Refactor: Extract useQuotes hook for retrieving quotes
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- pages/index.js
+++ pages/index.js
@@ -5,33 +5,28 @@ import Pagination from "../src/components/Pagination";
 import Quotes from "../src/components/Quotes";
 import Reset from "../src/components/Reset";
 import Search from "../src/components/Search";
-import useSWR from "swr";
+import useQuotes from "../src/hooks/useQuotes";
 import { useRouter } from "next/router";
 
 const Home = () => {
   const { query } = useRouter();
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data } = useSWR("/api/quotes", fetcher);
+  let { quotes } = useQuotes();
 
-  if (!data)
+  if (quotes === null)
     return (
       <Layout>
         <Loader />
       </Layout>
     );
 
-  let updatedQuotes = data.data.quotes;
-
-  updatedQuotes = updatedQuotes.sort(
-    (quoteA, quoteB) => quoteA.timestamp - quoteB.timestamp
-  );
+  quotes = quotes.sort((quoteA, quoteB) => quoteA.timestamp - quoteB.timestamp);
 
   const { search, filter = "All", page = 1 } = query;
 
   const searchedQuotes =
     search == null
-      ? updatedQuotes
-      : updatedQuotes?.filter((quote) =>
+      ? quotes
+      : quotes.filter((quote) =>
           quote.dialogue.some((message) =>
             message.text.toLowerCase().includes(search.toLowerCase())
           )
@@ -40,11 +35,11 @@ const Home = () => {
   const filteredQuotes =
     filter === "All"
       ? searchedQuotes
-      : searchedQuotes?.filter((quote) => quote.meta.workspacePage === filter);
+      : searchedQuotes.filter((quote) => quote.meta.workspacePage === filter);
 
   const pageSize = 5;
 
-  const paginatedQuotes = filteredQuotes?.slice(
+  const paginatedQuotes = filteredQuotes.slice(
     (page - 1) * pageSize,
     page * pageSize
   );
@@ -53,9 +48,9 @@ const Home = () => {
     <>
       <Layout title="Home">
         <Search style={{ marginBottom: "1rem" }} />
-        <Filter quotes={updatedQuotes} style={{ marginBottom: "1rem" }} />
+        <Filter quotes={quotes} style={{ marginBottom: "1rem" }} />
         <Reset />
-        <Quotes quotes={paginatedQuotes || []} />
+        <Quotes quotes={paginatedQuotes} />
         <Pagination pageSize={pageSize} totalCount={filteredQuotes.length} />
       </Layout>
     </>
---
+++ src/hooks/useQuotes.js
@@ -0,0 +1,16 @@
+import useSWR from "swr";
+
+const useQuotes = () => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data = { data: { quotes: null } }, error } = useSWR(
+    "/api/quotes",
+    fetcher
+  );
+
+  return {
+    quotes: data.data.quotes,
+    error,
+  };
+};
+
+export default useQuotes;
